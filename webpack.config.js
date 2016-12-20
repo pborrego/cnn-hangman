a87b3074a3f1a0a6205d452f7e1bb93acaa67609
Brian Duckett <brian.duckett@turner.com>
2016-12-20 00:14:17 -0500

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var config = require('webpack-es6-config');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = config({
+    filename: 'hangman.js',
+    libraryName: 'Hangman',
+    entry: ['./src/js/index.js'],
+    loaders: [{
+        test: /\.scss|\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')},
+    {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['es2015', 'react']
+        }
+    }],
+    plugins: [
+        new ExtractTextPlugin('hangman.css')
+    ]
+});
