fb7a309e99c64feb4b4a4c71187fa11ea0af014f
Brian Duckett <brian.duckett@turner.com>
2016-12-22 18:29:43 -0500

    v0.2.0 wip

---
+++ api/routes/index.js
@@ -0,0 +1,27 @@
+const config = require('../config');
+const word = require('random-words');
+
+
+
+exports.word = {
+    method: 'GET',
+    path: '/word',
+    config: {
+        id: 'sayWord',
+        handler: (req, reply) => {
+            const gameWord = word();
+            reply({
+                alphabet: config.alphabet,
+                blocks: new Array(gameWord.length),
+                guesses: [],
+                maxAttempts: config.maxAttempts,
+                miss: 0,
+                word: gameWord
+            });
+        },
+    }
+};
+
+
+
+module.exports = [exports.word];
